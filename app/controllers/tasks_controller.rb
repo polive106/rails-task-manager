3ebc0c90cd22ac5d6437347b843a97358723e130
Pierre Olive <pierre.olive4@gmail.com>
2021-02-09 12:35:49 +0100

    detail of a task view ok

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,5 +1,17 @@
 class TasksController < ApplicationController
+  before_action :set_task, only: [:show, :edit, :update, :destroy]
+
   def index
     @tasks = Task.all
+  end
+
+  def show
+  end
+
+
+  private
+
+  def set_task
+    @task = Task.find(params[:id])
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
   # => As a user, I can list tasks
   get 'tasks', to: 'tasks#index'
+  # => as a user I can see the details of a task
+  get 'tasks/:id', to: 'tasks#show', as: :task
 end
