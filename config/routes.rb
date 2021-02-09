69d39ce5c68ab659cf5e83fda40992e16f6e6924
Pierre Olive <pierre.olive4@gmail.com>
2021-02-09 12:20:57 +0100

    task index ok

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,4 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  # => As a user, I can list tasks
+  get 'tasks', to: 'tasks#index'
 end
