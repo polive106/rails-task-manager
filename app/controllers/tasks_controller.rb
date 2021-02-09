2e296e7f8e6868eee92257e7b699098acb74e710
Pierre Olive <pierre.olive4@gmail.com>
2021-02-09 14:28:48 +0100

    rails task manager skeleton and logic ok

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -8,10 +8,37 @@ class TasksController < ApplicationController
   def show
   end
 
+  def new
+    @task = Task.new
+  end
+
+  def create
+    @task = Task.new(task_params)
+    @task.save
+    redirect_to task_path(@task)
+  end
+
+  def edit
+  end
+
+  def update
+    @task.update(task_params)
+    @task.save
+    redirect_to tasks_path
+  end
+
+  def destroy
+    @task.destroy
+    redirect_to tasks_path
+  end
 
   private
 
   def set_task
     @task = Task.find(params[:id])
+  end
+
+  def task_params
+    params.require(:task).permit(:title, :details, :completed)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,18 @@
 Rails.application.routes.draw do
   # => As a user, I can list tasks
   get 'tasks', to: 'tasks#index'
+
+  # => as a user, I can add a new task
+  get 'tasks/new', to: 'tasks#new', as: :new_task
+  post 'tasks', to: 'tasks#create'
+
   # => as a user I can see the details of a task
   get 'tasks/:id', to: 'tasks#show', as: :task
+
+  # => as a user, I can edit a task
+  get 'tasks/:id/edit', to: 'tasks#edit', as: :edit_task
+  patch 'tasks/:id', to: 'tasks#update'
+
+  # => as a user, I can destroy a task
+  delete 'tasks/:id', to: 'tasks#destroy'
 end
